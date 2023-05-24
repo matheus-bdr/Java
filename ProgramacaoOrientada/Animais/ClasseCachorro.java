ae33ebe030f38b2a5a8601559607f3447c09d948
matheus-bdr <mmatheuscruzs@gmail.com>
2023-05-24 10:15:32 -0300

    commit

---
+++ ProgramacaoOrientada/Animais/ClasseCachorro.java
@@ -0,0 +1,10 @@
+
+public class ClasseCachorro {
+        //classe
+    public String nome; 
+    public String cor;
+    public int altura;
+    public double peso;
+    public int tamanhoDoRabo;
+}
+
---
+++ ProgramacaoOrientada/Animais/Objeto.java
@@ -0,0 +1,15 @@
+public class Objeto {
+    public static void main(String[] args) {
+        
+        ClasseCachorro cachorro1 = new ClasseCachorro();
+
+        cachorro1.nome = "gabs";
+        cachorro1.tamanhoDoRabo = 1;
+        cachorro1.cor = "marrom";
+        cachorro1.altura = 3;
+        cachorro1.peso = 2.2;
+
+    System.out.println(cachorro1);
+    }
+    
+}
